aaa68da94f27cc57b9befd8625bc7fab4e581cbc
ArmandoFigueiredo <armandopfigueiredo@gmail.com>
2022-08-20 09:54:41 -0300

    Quase Lá

--- JS/game.js
+++ JS/game.js
@@ -1,6 +1,84 @@
-altura = 0;
-largura = 0;
-vidas = 1;
+var altura = 0;
+var largura = 0;
+var vidas = 1;
+var tempo = 10;
+
+function tamanhoTela() {
+  altura = window.innerHeight;
+  largura = window.innerWidth;
+}
+
+tamanhoTela();
+
+var gameTime = setInterval(function(){          
+            tempo -= 1;
+
+            if(tempo < 0) {
+              clearInterval(gameTime);
+              clearInterval(criarMosquito);
+              window.location.href = "wingame.html";
+            } else { 
+              document.getElementById('gameTime').innerHTML = tempo;
+            }
+          }, 1000);                  
+
+function randomPosition() {
+  
+  if(document.getElementById('mosquitoTime')) {
+
+      document.getElementById('mosquitoTime').remove();  
+      
+      if(vidas > 3) {
+        window.location.href = "endgame.html";
+      } else {      
+      document.getElementById("v" + vidas).src="../IMAGENS/coracao_vazio.png";
+      vidas ++;
+      }
+}
+var posx = Math.floor(Math.random()*largura)-90;
+var posy = Math.floor(Math.random()*altura)-90;
+
+posx = posx < 0?0:posx;
+posy = posy < 0?0:posy;
+
+var mosquito = document.createElement('img');
+mosquito.src='imagem/mosquito.png';
+document.body.appendChild(mosquito);
+
+mosquito.className = tamanhoMosquito() + ' ' + aleatory();
+mosquito.style.left = posx + 'px';
+mosquito.style.top = posy + 'px';
+mosquito.style.position = 'absolute';
+mosquito.id = 'mosquitoTime';
+
+mosquito.onclick = function() {
+  this.remove();
+  }
+}
+
+function tamanhoMosquito() {
+  var classe = Math.floor(Math.random() * 3); 
+
+  switch(classe) {
+    case 0:
+      return 'mosq01';
+      case 1:
+        return 'mosq02';
+        case 2:
+          return 'mosq03';
+  }
+}
+
+function aleatory() {
+  var classe = Math.floor(Math.random() * 3); 
+
+  switch(classe) {
+    case 0:
+      return 'ladoA';
+      case 1:
+        return 'ladoB';        
+  }
+}
 
 function iniciarJogo() {
   nivel = document.getElementById("nivel").value;
@@ -28,22 +106,6 @@ function dificuldadeJogo() {
   }
 }
 
-function tamanhoTela() {
-  altura = window.innerHeight;
-  largura = window.innerWidth;
-}
-
-tamanhoTela();
 dificuldadeJogo();
 
-resetarTempo = setInterval(function() {
-  tempo -= 1;
 
-  if (tempo < 0) {
-    clearInterval(resetarTempo);
-    clearInterval(criarMosquitos);
-    window.location.href = "vitoria.html";
-  } else {
-    document.getElementById('tempoJogo').innerHTML = tempo;
-  }
-}, 1000);
