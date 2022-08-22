e9a4550e45e3122dcf0cdb4a1e515dd4955f84c2
ArmandoFigueiredo <armandopfigueiredo@gmail.com>
2022-08-22 18:47:12 -0300

    95% concluido

--- JS/game.js
+++ JS/game.js
@@ -1,84 +1,8 @@
-var altura = 0;
-var largura = 0;
-var vidas = 1;
-var tempo = 10;
-
-function tamanhoTela() {
-  altura = window.innerHeight;
-  largura = window.innerWidth;
-}
-
-tamanhoTela();
-
-var gameTime = setInterval(function(){          
-            tempo -= 1;
-
-            if(tempo < 0) {
-              clearInterval(gameTime);
-              clearInterval(criarMosquito);
-              window.location.href = "wingame.html";
-            } else { 
-              document.getElementById('gameTime').innerHTML = tempo;
-            }
-          }, 1000);                  
-
-function randomPosition() {
-  
-  if(document.getElementById('mosquitoTime')) {
-
-      document.getElementById('mosquitoTime').remove();  
-      
-      if(vidas > 3) {
-        window.location.href = "endgame.html";
-      } else {      
-      document.getElementById("v" + vidas).src="../IMAGENS/coracao_vazio.png";
-      vidas ++;
-      }
-}
-var posx = Math.floor(Math.random()*largura)-90;
-var posy = Math.floor(Math.random()*altura)-90;
-
-posx = posx < 0?0:posx;
-posy = posy < 0?0:posy;
-
-var mosquito = document.createElement('img');
-mosquito.src='../IMAGENS/pernilongo.png';
-document.body.appendChild(mosquito);
-
-mosquito.className = tamanhoMosquito() + ' ' + aleatory();
-mosquito.style.left = posx + 'px';
-mosquito.style.top = posy + 'px';
-mosquito.style.position = 'absolute';
-mosquito.id = 'mosquitoTime';
-
-mosquito.onclick = function() {
-  this.remove();
-  }
-}
-
-function tamanhoMosquito() {
-  var classe = Math.floor(Math.random() * 3); 
-
-  switch(classe) {
-    case 0:
-      return 'mosq01';
-      case 1:
-        return 'mosq02';
-        case 2:
-          return 'mosq03';
-  }
-}
-
-function aleatory() {
-  var classe = Math.floor(Math.random() * 3); 
-
-  switch(classe) {
-    case 0:
-      return 'ladoA';
-      case 1:
-        return 'ladoB';        
-  }
-}
+altura = 0;
+largura = 0;
+vidas = 1;
+abelhas_mortas = 0;
+mosquito_mortos = 0;
 
 function iniciarJogo() {
   nivel = document.getElementById("nivel").value;
@@ -86,7 +10,7 @@ function iniciarJogo() {
   if (nivel == "") {
     alert("Escolha um nível primeiro!");
   } else {
-    window.location.href = "game.html?" + nivel;
+    window.location.href = "../HTML/game.html?" + nivel;
   }
 }
 
@@ -106,4 +30,96 @@ function dificuldadeJogo() {
   }
 }
 
+function tamanhoTela() {
+  altura = window.innerHeight;
+  largura = window.innerWidth;
+}
+
+tamanhoTela();
 dificuldadeJogo();
+
+resetarTempo = setInterval(function() {
+  tempo -= 1;
+
+  if (tempo < 0) {
+    clearInterval(resetarTempo);
+    clearInterval(criarMosquitos);
+    
+    if(abelhas_mortas == 0)
+      window.location.href = "../HTML/win_game.html";
+    else
+      window.location.href = "../HTML/game_over.html";
+      
+  } else {
+    document.getElementById('tempoJogo').innerHTML = tempo;
+  }
+}, 1000);
+
+function posicaoMosquito() {
+  posx = Math.round(Math.random() * largura) - 90;
+  posy = Math.round(Math.random() * altura) - 90;
+  posx = posx < 0?0:posx;
+  posy = posy < 0?0:posy;
+
+  return posx + "px, " + posy + "px";
+}
+
+function tamanhoMosquito() {
+  return Math.round(Math.random() * (3 - 1) + 1);
+}
+
+function direcaoMosquito() {
+  return Math.round(Math.random() < 0.5 ? -1:1);
+}
+
+function gerarMosquito() {
+  if (document.getElementById('tempoMosquito')) {
+    document.getElementById('tempoMosquito').remove();
+
+    if (vidas <= 0) {
+      window.location.href = "../HTML/game_over.html";
+    } else {
+      vidaVazia = document.getElementById('v' + vidas);
+      vidaVazia.src = "../IMAGENS/coracao_vazio.png";
+      vidaVazia.draggable = false;
+      vidas++;
+    }
+  }
+
+  mosquito = document.createElement('img');
+  mosquito.src = '../IMAGENS/pernilongo.png';
+  mosquito.className = 'mosq0' + tamanhoMosquito();
+  mosquito.style.position = 'absolute';
+  mosquito.style.transform = "translate(" + posicaoMosquito() + ") scaleX(" + direcaoMosquito() + ")";
+  mosquito.id = 'tempoMosquito';
+  mosquito.draggable = false;
+  mosquito.onclick = function () {
+    new Audio("../SONS/sons_SPLAT.mp3").play();
+    this.remove();
+    mosquito_mortos++;
+  }
+
+  document.body.appendChild(mosquito);
+}
+
+function gerarAbelhas() {
+
+  abelha = document.createElement('img');
+  abelha.src = '../IMAGENS/abelha_1.png';
+  abelha.className = 'mosq0' + tamanhoMosquito();
+  abelha.style.position = 'absolute';
+  abelha.style.transform = "translate(" + posicaoMosquito() + ") scaleX(" + direcaoMosquito() + ")";
+  abelha.id = 'tempoMosquito';
+  abelha.draggable = false;
+  abelha.onclick = function () {
+    new Audio("../SONS/sons_SPLAT.mp3").play();
+    this.remove();
+    abelhas_mortas++;
+
+    if(abelhas_mortas >= 3) {
+      window.location.href = "../HTML/game_over.html"; 
+    }
+  }
+
+  document.body.appendChild(abelha);
+}
